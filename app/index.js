40cdcdb276ac5ec35cae32a8877fb58186960177
saurabh kamble <saurabh@saurabhs-MacBook-Pro.local>
2017-06-26 00:45:37 +0530

    adding components into react app

--- app/index.js
+++ app/index.js
@@ -4,64 +4,10 @@ var PropTypes = require('prop-types');
 
 require('./index.css');
 
-class Users extends React.Component {
-  render() {
+var App = require('./components/app');
 
-      var friends = this.props.list.filter(function(user){
-        return user.friend === true;
-    });
-
-
-
-    var nonfriends = this.props.list.filter(function(user){
-        return user.friend !== true;
-    });
-
-    return (
-      <div>
-        <h1>Friends</h1>
-        <ul>
-            {
-              friends.map(function(user){
-                
-                return <li key={user.name}>{user.name}</li>
-              
-              })
-            }
-        </ul>
-        
-        <hr />
-        
-        <h1> Non Friends </h1>
-        <ul>
-          {
-              nonfriends.map(function(user){
-
-                return <li key={user.name}>{user.name} </li>  
-
-              })
-            }
-        </ul>        
-      </div>
-    )
-  }
-}
-
-Users.propTypes = {
-    list:PropTypes.arrayOf(PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      friend:PropTypes.bool.isRequired,
-    })),
-}
 
 ReactDOM.render(
-  <Users list={[
-    { name: 'Tyler', friend: true },
-    { name: 'Ryan', friend: true },
-    { name: 'Michael', friend: false },
-    { name: 'Mikenzi', friend: false },
-    { name: 'Jessica', friend: true },
-    { name: 'Dan', friend: 'false' } ]}  
-  />,
+  <App />,
   document.getElementById('app')
 );
